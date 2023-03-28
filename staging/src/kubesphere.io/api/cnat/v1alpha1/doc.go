42ca7223a75ac581496e14a9e2ae3d7fe1054848
Charles Chou <charles_chou@accton.com>
2023-03-28 07:40:52 +0000

    Prepare basic files for cnat

---
+++ staging/src/kubesphere.io/api/cnat/v1alpha1/doc.go
@@ -0,0 +1,8 @@
+// Package v1alpha1 contains API Schema definitions for the tower v1alpha1 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=kubesphere.io/api/cnat
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=cnat.programming-kubernetes.info
+
+package v1alpha1
---
+++ staging/src/kubesphere.io/api/cnat/v1alpha1/register.go
@@ -0,0 +1,26 @@
+// Package v1alpha1 contains API Schema definitions for the tower v1alpha1 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=cnat.programming-kubernetes.info
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: "cnat.programming-kubernetes.info", Version: "v1alpha1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
