a67b34d6f70ac1242e4ef6e24bda918be05696da
Charles Chou <charles_chou@accton.com>
2023-04-27 02:00:01 +0000

    Update virtual machine CR

--- staging/src/kubesphere.io/api/virtualization/v1alpha1/virtualmachine_types.go
+++ staging/src/kubesphere.io/api/virtualization/v1alpha1/virtualmachine_types.go
@@ -9,24 +9,29 @@ import (
 // +k8s:openapi-gen=true
 // +genclient:nonNamespaced
 
-const (
-	PhasePending = "PENDING"
-	PhaseRunning = "RUNNING"
-	PhaseDone    = "DONE"
-)
-
 const VirtualMachineFinalizer = "finalizers.virtualization.kubesphere.io/virtualmachine"
+
+type Image struct {
+	Name      string `json:"name,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
+}
+
+type Source struct {
+	Image Image `json:"image,omitempty"`
+}
 
 type Requests struct {
 	Storage string `json:"storage,omitempty"`
 }
 
-type Resources struct {
+type DVResources struct {
 	Requests Requests `json:"requests,omitempty"`
 }
 
 type DiskVolumeTemplateSpec struct {
-	Resources Resources `json:"resources,omitempty"`
+	// Resources represents the minimum resources the volume should have.
+	Resources DVResources `json:"resources,omitempty"`
+	Source    Source      `json:"source,omitempty"`
 }
 
 type DiskVolumeTemplateStatus struct {
@@ -35,18 +40,79 @@ type DiskVolumeTemplateStatus struct {
 type DiskVolumeTemplate struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	// DiskVolumeSpec is the spec for a DiskVolume resource
 	Spec   DiskVolumeTemplateSpec   `json:"spec,omitempty"`
 	Status DiskVolumeTemplateStatus `json:"status,omitempty"`
+}
+
+type Cpu struct {
+	Cores int32 `json:"cores,omitempty"`
+}
+
+type MacVtap struct {
+}
+
+type Interface struct {
+	MacVtap MacVtap `json:"macvtap,omitempty"`
+	Name    string  `json:"name,omitempty"`
+}
+
+type Devices struct {
+	Interfaces []Interface `json:"interfaces,omitempty"`
+}
+
+type Limits struct {
+	Memory string `json:"memory,omitempty"`
+}
+
+type DomainResources struct {
+	Limits Limits `json:"limits,omitempty"`
+}
+
+type Domain struct {
+	Cpu       Cpu             `json:"cpu,omitempty"`
+	Devices   Devices         `json:"devices,omitempty"`
+	Resources DomainResources `json:"resources,omitempty"`
+}
+
+type Multus struct {
+	NetworkName string `json:"networkName,omitempty"`
+}
+
+type Network struct {
+	Multus Multus `json:"multus,omitempty"`
+	Name   string `json:"name,omitempty"`
+}
+
+type CloudInitNoCloud struct {
+	UserDataBase64 string `json:"userDataBase64,omitempty"`
+}
+
+type Volume struct {
+	CloudInitNoCloud CloudInitNoCloud `json:"cloudInitNoCloud,omitempty"`
+	Name             string           `json:"name,omitempty"`
+}
+
+type Hardware struct {
+	Domain           Domain    `json:"domain,omitempty"`
+	EvictionStrategy string    `json:"evictionStrategy,omitempty"`
+	Hostname         string    `json:"hostname,omitempty"`
+	Networks         []Network `json:"networks,omitempty"`
+	Volumes          []Volume  `json:"volumes,omitempty"`
 }
 
 // VirtualMachineSpec defines the desired state of VirtualMachine
 type VirtualMachineSpec struct {
 	// DiskVolumeTemplate is the name of the DiskVolumeTemplate.
 	DiskVolumeTemplates []DiskVolumeTemplate `json:"diskVolumeTemplates,omitempty"`
+	// DiskVolume is the name of the DiskVolume.
+	DiskVolumes []string `json:"diskVolumes,omitempty"`
 	// Name is the name of the VirtualMachine.
 	Name string `json:"name,omitempty"`
 	// Memory is the memory of the VirtualMachine.
 	Memory string `json:"memory,omitempty"`
+	// Hardware is the hardware of the VirtualMachine.
+	Hardware Hardware `json:"hardware,omitempty"`
 }
 
 // VirtualMachineStatus defines the observed state of VirtualMachine
